41f62bd4aa2a973fcb456607313635c59ffa5b69
Diego Díaz <diego_diaz9990@hotmail.com>
2022-07-27 13:15:12 -0300

    pokedex frontend

--- src/Componentes/Detalles/Detalles.jsx
+++ src/Componentes/Detalles/Detalles.jsx
@@ -42,14 +42,11 @@ export default function Detalles() {
         }
         setListaPokemons(await respuesta2.json());
       } catch (error) {
+        navigate("/404", { replace: true });
         console.log("No se pudo conectar con el backend");
       }
     };
     cargarPokemon();
-    const resultado = listaPokemons.findIndex(
-      (pkmn) => pkmn.nombre.toLowerCase() === pokemon.nombre.toLowerCase()
-    );
-    setIndicePokemon(resultado);
     setLoading(false);
   }, [id]);
 
@@ -57,223 +54,211 @@ export default function Detalles() {
     const resultado = listaPokemons.findIndex(
       (pkmn) => pkmn.nombre.toLowerCase() === pokemon.nombre.toLowerCase()
     );
-    if (resultado) {
-      setIndicePokemon(resultado);
-    }
-    console.log("paso");
+
+    setIndicePokemon(resultado);
   }, [listaPokemons, pokemon]);
 
-  if (id !== pokemon.nombre && !loading) {
-    navigate("/404", { replace: true });
-  } else
-    return (
-      <>
-        <div className="contenedor">
-          <div
-            className="tarjetaPokDetalle"
-            style={{ backgroundColor: pokemon.colorPrimario }}
-          >
-            {listaPokemons[indicePokemon - 1] && (
+  return (
+    <>
+      <div className="contenedor">
+        <div
+          className="tarjetaPokDetalle"
+          style={{ backgroundColor: pokemon.colorPrimario }}
+        >
+          {indicePokemon > 0 && (
+            <div
+              onClick={() =>
+                navigate(`/${listaPokemons[indicePokemon - 1]?.nombre}`, {
+                  replace: true,
+                })
+              }
+              className="vector1"
+            >
+              <img src={ArrowBefore} alt="" />
+            </div>
+          )}
+
+          {listaPokemons[indicePokemon + 1] && (
+            <div
+              onClick={() =>
+                navigate(`/${listaPokemons[indicePokemon + 1].nombre}`, {
+                  replace: true,
+                })
+              }
+              className="vector2"
+            >
+              <img src={ArrowNext} alt="" />
+            </div>
+          )}
+
+          <div className="nombrePokemon">
+            <Link to={"/"}>
+              <div className="imgFlecha">
+                <img src={ArrowLeft} alt="flecha-atras" />
+              </div>
+            </Link>
+
+            <div className="pokeName">
+              <h1 className="nombre">{pokemon.nombre}</h1>
+              <h2 className="h2">{pokemon.id}</h2>
+            </div>
+          </div>
+          <div className="imageFondo">
+            <img src={Pokeball} alt="pokeball" />
+          </div>
+          <div className="imagenPoke">
+            <img className="imgPoke" src={pokemon.imagen} alt="foto pokemon" />
+          </div>
+
+          <div className="card">
+            <div className="cardType">
               <div
-                onClick={() =>
-                  navigate(`/${listaPokemons[indicePokemon - 1].nombre}`, {
-                    replace: true,
-                  })
-                }
-                className="vector1"
+                className="cuadro1"
+                style={{ background: pokemon.colorPrimario }}
               >
-                <img src={ArrowBefore} alt="" />
+                {pokemon.tipo1}
               </div>
-            )}
-
-            {listaPokemons[indicePokemon + 1] && (
-              <div
-                onClick={() =>
-                  navigate(`/${listaPokemons[indicePokemon + 1].nombre}`, {
-                    replace: true,
-                  })
-                }
-                className="vector2"
-              >
-                <img src={ArrowNext} alt="" />
-              </div>
-            )}
-
-            <div className="nombrePokemon">
-              <Link to={"/"}>
-                <div className="imgFlecha">
-                  <img src={ArrowLeft} alt="flecha-atras" />
-                </div>
-              </Link>
-
-              <div className="pokeName">
-                <h1 className="nombre">{pokemon.nombre}</h1>
-                <h2 className="h2">{pokemon.id}</h2>
-              </div>
-            </div>
-            <div className="imageFondo">
-              <img src={Pokeball} alt="pokeball" />
-            </div>
-            <div className="imagenPoke">
-              <img
-                className="imgPoke"
-                src={pokemon.imagen}
-                alt="foto pokemon"
-              />
-            </div>
-
-            <div className="card">
-              <div className="cardType">
+              {pokemon.colorSecundario !== "" ? (
                 <div
-                  className="cuadro1"
-                  style={{ background: pokemon.colorPrimario }}
+                  className="cuadro2"
+                  style={{ background: pokemon.colorSecundario }}
                 >
-                  {pokemon.tipo1}
+                  {pokemon.tipo2}
                 </div>
-                {pokemon.colorSecundario !== "" ? (
-                  <div
-                    className="cuadro2"
-                    style={{ background: pokemon.colorSecundario }}
-                  >
-                    {pokemon.tipo2}
-                  </div>
-                ) : (
-                  <div></div>
-                )}
+              ) : (
+                <div></div>
+              )}
+            </div>
+
+            <div className="cardTitle" style={{ color: pokemon.colorPrimario }}>
+              <h3>About</h3>
+            </div>
+
+            <div className="cardBout">
+              <div className="cardWeight">
+                <div className="cardImgkg">
+                  <img src={Weight} alt="balanza" />
+                  <p>{pokemon.peso / 10} kg</p>
+                </div>
+                <div className="cardWeightTitle">
+                  <p>Wheight</p>
+                </div>
               </div>
 
-              <div
-                className="cardTitle"
+              <div className="cardHeight">
+                <div className="cardImgM">
+                  <img src={Height} alt="regla" />
+                  <p>{pokemon.altura / 10} m</p>
+                </div>
+                <div className="cardHeightTitle">
+                  <p>Height</p>
+                </div>
+              </div>
+
+              <div className="cardMoves">
+                <div className="cardText1">
+                  <p>
+                    {pokemon.movimiento1} <br />
+                    {pokemon.movimiento2}
+                  </p>
+                </div>
+                <div className="cardTextMoves">
+                  <p>Moves</p>
+                </div>
+              </div>
+            </div>
+            <div className="cardText2">
+              <p>{pokemon.descripcion}</p>
+            </div>
+            <div className="cardTitleGraph">
+              <p
+                className="pCardTitle"
                 style={{ color: pokemon.colorPrimario }}
               >
-                <h3>About</h3>
-              </div>
+                Base Stats
+              </p>
+            </div>
 
-              <div className="cardBout">
-                <div className="cardWeight">
-                  <div className="cardImgkg">
-                    <img src={Weight} alt="balanza" />
-                    <p>{pokemon.peso / 10} kg</p>
-                  </div>
-                  <div className="cardWeightTitle">
-                    <p>Wheight</p>
-                  </div>
-                </div>
-
-                <div className="cardHeight">
-                  <div className="cardImgM">
-                    <img src={Height} alt="regla" />
-                    <p>{pokemon.altura / 10} m</p>
-                  </div>
-                  <div className="cardHeightTitle">
-                    <p>Height</p>
-                  </div>
-                </div>
-
-                <div className="cardMoves">
-                  <div className="cardText1">
-                    <p>
-                      {pokemon.movimiento1} <br />
-                      {pokemon.movimiento2}
-                    </p>
-                  </div>
-                  <div className="cardTextMoves">
-                    <p>Moves</p>
-                  </div>
-                </div>
+            <div className="contenedorGraph">
+              <div
+                className="atributtes"
+                style={{ color: pokemon.colorPrimario }}
+              >
+                <p>HP</p>
+                <p>ATK</p>
+                <p>DEF</p>
+                <p>SATK</p>
+                <p>SDEF</p>
+                <p>SPD</p>
               </div>
-              <div className="cardText2">
-                <p>{pokemon.descripcion}</p>
+              <div className="values">
+                <p>{pokemon.HP}</p>
+                <p>{pokemon.ATK}</p>
+                <p>{pokemon.DEF}</p>
+                <p>{pokemon.SATK}</p>
+                <p>{pokemon.SDEF}</p>
+                <p>{pokemon.SPD}</p>
               </div>
-              <div className="cardTitleGraph">
-                <p
-                  className="pCardTitle"
-                  style={{ color: pokemon.colorPrimario }}
-                >
-                  Base Stats
-                </p>
-              </div>
-
-              <div className="contenedorGraph">
-                <div
-                  className="atributtes"
-                  style={{ color: pokemon.colorPrimario }}
-                >
-                  <p>HP</p>
-                  <p>ATK</p>
-                  <p>DEF</p>
-                  <p>SATK</p>
-                  <p>SDEF</p>
-                  <p>SPD</p>
+              <div className="graph">
+                <div className="graphlinea">
+                  <div
+                    className="barra1"
+                    style={{
+                      backgroundColor: pokemon.colorPrimario,
+                      width: `${pokemon.HP / 2}%`,
+                    }}
+                  ></div>
                 </div>
-                <div className="values">
-                  <p>{pokemon.HP}</p>
-                  <p>{pokemon.ATK}</p>
-                  <p>{pokemon.DEF}</p>
-                  <p>{pokemon.SATK}</p>
-                  <p>{pokemon.SDEF}</p>
-                  <p>{pokemon.SPD}</p>
+                <div className="graphlinea">
+                  <div
+                    className="barra2"
+                    style={{
+                      backgroundColor: pokemon.colorPrimario,
+                      width: `${pokemon.ATK / 2}%`,
+                    }}
+                  ></div>
                 </div>
-                <div className="graph">
-                  <div className="graphlinea">
-                    <div
-                      className="barra1"
-                      style={{
-                        backgroundColor: pokemon.colorPrimario,
-                        width: `${pokemon.HP / 2}%`,
-                      }}
-                    ></div>
-                  </div>
-                  <div className="graphlinea">
-                    <div
-                      className="barra2"
-                      style={{
-                        backgroundColor: pokemon.colorPrimario,
-                        width: `${pokemon.ATK / 2}%`,
-                      }}
-                    ></div>
-                  </div>
-                  <div className="graphlinea">
-                    <div
-                      className="barra3"
-                      style={{
-                        backgroundColor: pokemon.colorPrimario,
-                        width: `${pokemon.DEF / 2}%`,
-                      }}
-                    ></div>
-                  </div>
-                  <div className="graphlinea">
-                    <div
-                      className="barra4"
-                      style={{
-                        backgroundColor: pokemon.colorPrimario,
-                        width: `${pokemon.SATK / 2}%`,
-                      }}
-                    ></div>
-                  </div>
-                  <div className="graphlinea">
-                    <div
-                      className="barra5"
-                      style={{
-                        backgroundColor: pokemon.colorPrimario,
-                        width: `${pokemon.SDEF / 2}%`,
-                      }}
-                    ></div>
-                  </div>
-                  <div className="graphlinea">
-                    <div
-                      className="barra6"
-                      style={{
-                        backgroundColor: pokemon.colorPrimario,
-                        width: `${pokemon.SPD / 2}%`,
-                      }}
-                    ></div>
-                  </div>
+                <div className="graphlinea">
+                  <div
+                    className="barra3"
+                    style={{
+                      backgroundColor: pokemon.colorPrimario,
+                      width: `${pokemon.DEF / 2}%`,
+                    }}
+                  ></div>
+                </div>
+                <div className="graphlinea">
+                  <div
+                    className="barra4"
+                    style={{
+                      backgroundColor: pokemon.colorPrimario,
+                      width: `${pokemon.SATK / 2}%`,
+                    }}
+                  ></div>
+                </div>
+                <div className="graphlinea">
+                  <div
+                    className="barra5"
+                    style={{
+                      backgroundColor: pokemon.colorPrimario,
+                      width: `${pokemon.SDEF / 2}%`,
+                    }}
+                  ></div>
+                </div>
+                <div className="graphlinea">
+                  <div
+                    className="barra6"
+                    style={{
+                      backgroundColor: pokemon.colorPrimario,
+                      width: `${pokemon.SPD / 2}%`,
+                    }}
+                  ></div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </>
-    );
+      </div>
+    </>
+  );
 }
